83cbd8ee23bc0bf407c4d0c63868c6488e31c638
Alex <proallex@icloud.com>
2016-04-16 17:22:57 +0300

    Added GameProcess class.

---
+++ Client/Runner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Runner
+    {
+        public static void Main(string[] args)
+        {
+            GameProcess game = new GameProcess("test.exe");
+            game.Start();
+
+            if (game.IsRunning())
+            {
+                game.WaitForExit();
+            }
+        }
+    }
+}
